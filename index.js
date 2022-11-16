76fcaf527b6b4dc7837e1e3562778d972ce81b1f
Patricio Uskaer <patriciouskaer@gmail.com>
2022-11-16 14:09:44 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+//                       _oo0oo_
+//                      o8888888o
+//                      88" . "88
+//                      (| -_- |)
+//                      0\  =  /0
+//                    ___/`---'\___
+//                  .' \\|     |// '.
+//                 / \\|||  :  |||// \
+//                / _||||| -:- |||||- \
+//               |   | \\\  -  /// |   |
+//               | \_|  ''\---/''  |_/ |
+//               \  .-\__  '-'  ___/-. /
+//             ___'. .'  /--.--\  `. .'___
+//          ."" '<  `.___\_<|>_/___.' >' "".
+//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+//         \  \ `_.   \_ __\ /__ _/   .-` /  /
+//     =====`-.____`.___ \_____/___.-`___.-'=====
+//                       `=---='
+//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+require('dotenv').config();
+const server = require('./src/app.js');
+const { conn } = require('./src/db.js');
+const port = process.env.PORT || 3001;
+
+conn.sync({ force: false }).then(() => {
+  
+    server.listen(port, () => {
+   
+    console.log(`Server raised in port${port}`);
+  });
+});
+
+/* const server = require('./src/app.js');
+const { conn } = require('./src/db.js');
+conn.sync({ force: false }).then(() => {
+  server.listen(3001, () => {
+    console.log('%s listening at 3001');
+  });
+}); */
