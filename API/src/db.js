c5769fc6176fc85d91171d6d705e9595ea4e00c0
Patricio Uskaer <patriciouskaer@gmail.com>
2022-11-16 13:48:02 -0300

    deploy

--- API/src/db.js
+++ API/src/db.js
@@ -11,10 +11,21 @@ const {
   native: false, 
 }); */
 /* const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, { */
-const sequelize = new Sequelize(
+/* const sequelize = new Sequelize(
 DB_DEPLOY, {
+  logging: false,
+  native: false, 
+}); */
+const sequelize = new Sequelize( DB_DEPLOY, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  //CONFIGURACION ADICIONAL
+  dialectOptions:{
+    ssl:{
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
 const basename = path.basename(__filename);
 
