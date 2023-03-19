bc60dde014ea8bb5d695742f6e51bb104c5bc05e
Patricio Uskaer <patriciouskaer@gmail.com>
2023-03-19 08:34:52 -0300

    deploy

--- src/db.js
+++ src/db.js
@@ -16,7 +16,7 @@ DB_DEPLOY, {
   logging: false,
   native: false, 
 }); */
-const sequelize = new Sequelize( DB_DEPLOY, {
+const sequelize = new Sequelize( {DB_DEPLOY}, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   //CONFIGURACION ADICIONAL
