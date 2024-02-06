0eedbcb5fc9e1f3d58445141f0579805e5bbac74
Patricio Uskaer <patriciouskaer@gmail.com>
2024-02-05 21:34:16 -0300

    d

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const port = process.env.PORT || 3001;
 
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   
     server.listen(port, () => {
    
