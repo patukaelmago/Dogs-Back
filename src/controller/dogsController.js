2dce1eff5f2c05e75cba97a07707c09dc3dd36d3
Patricio Uskaer <patriciouskaer@gmail.com>
2024-02-09 12:12:56 -0300

    img

--- src/controller/dogsController.js
+++ src/controller/dogsController.js
@@ -5,25 +5,27 @@ require('dotenv').config();
 
 
 async function getApiDogs(){
-    const getApi = await axios('https://api.thedogapi.com/v1/breeds?live_pE8PgnzSZLtTCj2UwX3XPmbk5gmwL0BYfvGpeHlfYNPZwWhObxyDUPSRY7m5UL6O')
-    // const  image = await axios("https://api.thedogapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1") 
-    const getApiInfo = await getApi?.data.map(d => {
-        return {
-            id:d.id,
-            name:d.name,
-            image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/132.png",
-            // image:image,
-            temperaments: typeof(d.temperament) === 'string'? d.temperament.split(', '):d.temperament ,
-            lifespan:d.life_span,
-            weightMin:Number.isNaN(parseInt(d.weight.metric.split(' - ')[0]))  ? 20 :parseInt(d.weight.metric.split(' - ')[0]),
-            weightMax:Number.isNaN(parseInt(d.weight.metric.split(' - ')[1]))  ? 50 : parseInt(d.weight.metric.split(' - ')[1]),
-            heightMin:Number.isNaN(parseInt(d.height.metric.split(' - ')[0])) ? 1 :parseInt(d.height.metric.split(' - ')[0]),
-            heightMax:Number.isNaN(parseInt(d.height.metric.split(' - ')[1])) ? 10 : parseInt(d.height.metric.split(' - ')[1]),
-            createdInDb: false
-        }
-    });
+       const dogsApi = await axios.get(`https://api.thedogapi.com/v1/breeds?live&limit=24&page=0_pE8PgnzSZLtTCj2UwX3XPmbk5gmwL0BYfvGpeHlfYNPZwWhObxyDUPSRY7m5UL6O`)
+        const dogs = await dogsApi.data.map(dogs => {return axios.get(`https://api.thedogapi.com/v1/images/${dogs.reference_image_id}`)});
+        const dogsPromise = await Promise.all(dogs);  
+        const result = dogsPromise.map(d =>d.data)
+
+        const getApiInfo = await dogsApi?.data.map((d) => {
+         return {
+                        id:d.id,
+                        name:d.name,
+                        image:result.find(({id}) => id == d.reference_image_id).url,
+                        temperaments: typeof(d.temperament) === 'string'? d.temperament.split(', '):d.temperament ,
+                        lifespan:d.life_span,
+                        weightMin:Number.isNaN(parseInt(d.weight.metric.split(' - ')[0]))  ? 20 :parseInt(d.weight.metric.split(' - ')[0]),
+                        weightMax:Number.isNaN(parseInt(d.weight.metric.split(' - ')[1]))  ? 50 : parseInt(d.weight.metric.split(' - ')[1]),
+                        heightMin:Number.isNaN(parseInt(d.height.metric.split(' - ')[0])) ? 1 :parseInt(d.height.metric.split(' - ')[0]),
+                        heightMax:Number.isNaN(parseInt(d.height.metric.split(' - ')[1])) ? 10 : parseInt(d.height.metric.split(' - ')[1]),
+                        createdInDb: false
+                    }
+                });
     return getApiInfo;
-}
+            }
 
 
 async function getDbDogs(){
